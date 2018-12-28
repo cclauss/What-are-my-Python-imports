a19ff46978cfed2a56f5daacd7ae3680fcbcda37
cclauss <cclauss@me.com>
2018-12-28 09:32:14 +0100

    Update find_imports.py

--- find_imports.py
+++ find_imports.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 # TODO: This is NOT an exhaustive list but works for one particular project
-STD_LIB = """ConfigParser Cookie HTMLParser Queue StringIO __future__ argparse array atexit
+STD_LIB = """ConfigParser Cookie HTMLParser Queue StringIO __future__ argparse
+    array atexit
 base64 bz2
 cPickle cStringIO calendar cgi codecs collections compress cookielib copy csv
 datetime doctest
@@ -17,7 +18,8 @@ numbers
 optparse os
 pdb pydoc random
 re
-sched sendmail shelve shlex shutil site socket stat string subcommand subprocess sys
+sched sendmail shelve shlex shutil site socket stat string subcommand
+    subprocess sys
 tempfile threading time traceback types
 unicodedata unittest urllib urllib2 uuid
 warnings
@@ -31,6 +33,7 @@ lines = (c.strip().replace(',', ' ').replace('.', ' ') for a, b, c in lines
          if b and c.startswith(('from ', 'import '))
          and not a.startswith('scripts/20'))
 lines = sorted(set(line.split()[1] for line in lines
-                   if not line.split()[1].startswith(('infogami', 'openlibrary')
-                   and line.split()[1] not in STD_LIB)))
+                   if line.split()[1] not in STD_LIB
+                   and not line.split()[1].startswith(('infogami',
+                                                       'openlibrary'))))
 print('\n'.join(lines))
